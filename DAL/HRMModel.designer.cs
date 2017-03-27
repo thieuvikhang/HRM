c559b0f555f2a3ac4de02116706cf816662b38cd
thieuvikhang <thieuvikhang@gmail.com>
2017-03-27 09:26:20 +0700

    Up

--- DAL/HRMModel.designer.cs
+++ DAL/HRMModel.designer.cs
@@ -75,7 +75,7 @@ namespace DAL
     #endregion
 		
 		public HRMModelDataContext() : 
-				base(global::DAL.Properties.Settings.Default.HRMConnectionString, mappingSource)
+				base(global::DAL.Properties.Settings.Default.HRMConnectionString1, mappingSource)
 		{
 			OnCreated();
 		}
